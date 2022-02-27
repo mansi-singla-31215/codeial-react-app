36cc0463efbd36dc40da6e1bf65d1e404a9e1c4a
mansisingla31215@gmail.com <83231180+mansi-singla-31215@users.noreply.github.com>
2022-02-27 19:24:55 +0530

    Commit

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+import { useAuth } from '../hooks';
+import {Home, Login, Signup, Settings} from '../pages';
+import {Loader, Navbar} from './';
+
+const Page404 = () => {
+  return <h1>404</h1>
+};
+
+function App() {
+  const auth = useAuth();
+
+  if(auth.loading){
+    return <Loader />
+  }
+
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home/>} /> 
+          <Route exact path="/login" element={<Login />} /> 
+          <Route exact path="/register" element={<Signup />} /> 
+          <Route exact path="/settings" element={<Settings />} />    
+          <Route path="*" element={<Page404 />} />  
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,83 @@
+import PropTypes from 'prop-types';
+import {Comment, Loader} from '../components';
+import { getPosts } from '../api';
+import styles from '../styles/home.module.css';
+import { useState, useEffect } from 'react';
+
+const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await getPosts();
+
+      if (response.success) {
+        setPosts(response.data.posts);
+      }
+
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
+
+  if(loading){
+    return <Loader />
+  }
+
+  return (
+    <div className={styles.postsList}>
+      {posts.map((post) => (
+        <div className={styles.postWrapper} key={`post-${post._id}`}>
+          <div className={styles.postHeader}>
+            <div className={styles.postAvatar}>
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/3237/3237472.png"
+                alt="user-pic"
+              />
+              <div>
+                <span className={styles.postAuthor}>{post.user.name}</span>
+                <span className={styles.postTime}>a minute ago</span>
+              </div>
+            </div>
+            <div className={styles.postContent}>{post.content}</div>
+
+            <div className={styles.postActions}>
+              <div className={styles.postLike}>
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/1077/1077035.png"
+                  alt="likes-icon"
+                />
+                <span>5</span>
+              </div>
+
+              <div className={styles.postCommentsIcon}>
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/1380/1380338.png"
+                  alt="comments-icon"
+                />
+                <span>{post.comments.length}</span>
+              </div>
+            </div>
+            <div className={styles.postCommentBox}>
+              <input placeholder="Start typing a comment" />
+            </div>
+
+            <div className={styles.postCommentsList}>
+              {post.comments.map((comment) => (
+                <Comment comment = {comment} />
+              ))}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+Home.propTypes = {
+  posts: PropTypes.array.isRequired
+}
+
+export default Home;
